b69d559f75055750a9864bb15e261103f92b2385
bekramb <bekramb@gmail.com>
2017-08-01 23:42:13 +0300

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE FROM meals;
+ALTER SEQUENCE global_seq2 RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password');
@@ -11,3 +13,9 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories)
+VALUES ('2015-06-01 14:00', 'Админ ланч', 510);
+
+INSERT INTO meals (datetime, description, calories)
+VALUES ('2015-06-01 21:00', 'Админ ланч', 1500);
